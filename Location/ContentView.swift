3f57eeb22931aa9a50a529216f74aecf03f1dfac
mabinglei <mabinglei183@gmail.com>
2025-03-01 13:25:53 +0800

    Initial Commit

---
+++ Location/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Location
+//
+//  Created by Binglei Ma on 3/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Binglei Ma on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
