db458d2f9d5ae0814de049ac5e7480447b914bd6
mabinglei <mabinglei183@gmail.com>
2025-03-01 15:40:22 +0800

    init

---
+++ Location/View/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Location
+//
+//  Created by Binglei Ma on 3/1/25.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    @StateObject private var locationManager = LocationManager()
+    var body: some View {
+        Map(position: $locationManager.location) {
+            UserAnnotation()
+        }
+    }
+}
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private var locationManager = CLLocationManager()
+    private let span = MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+    @Published var location: MapCameraPosition
+    
+    override init() {
+        self.location = MapCameraPosition.region(.init(center: .init(), span: span))
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let newLocation = locations.last else { return }
+        withAnimation(.easeInOut(duration: 1.0)) {
+            self.location = MapCameraPosition.region(.init(center: newLocation.coordinate, span: span))
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
